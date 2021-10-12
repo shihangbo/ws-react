9f773f42e39fe46b9c3ec51ba5447b0835325420
shihangbo <shihangbo@126.com>
2021-10-12 19:19:06 +0800

    feat:thunk使用

--- src/components/Counter1.tsx
+++ src/components/Counter1.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import {Component} from 'react'
 import {connect} from 'react-redux'
-import {Dispatch} from 'redux'
+import { Dispatch } from 'redux';
 import { CombinedState } from '@/store/reducers'
 import { Counter1State } from '@/store/reducers/counter1'
 import * as types from '@/store/action-types';
@@ -10,9 +10,18 @@ import { push } from 'connected-react-router'
 // 1 编写组件
 // 2 连接仓库
 let mapStateToProps = (state:CombinedState):Counter1State => state.counter1
-let mapDispathToProps = (dispatch:Dispatch) => ({
+let mapDispathToProps = (dispatch:any) => ({
   add() {
     dispatch({type: types.ADD1})
+  },
+  // thunk 使用
+  asyncAdd() {
+    dispatch((dispatch:any, getState:any) => {
+      setTimeout(() => {
+        dispatch({type: types.ADD1})
+        console.log(1111, getState())
+      }, 1000)
+    })
   },
   goto(location:LocationDescriptorObject<LocationState>) {
     dispatch(push(location))
@@ -25,6 +34,7 @@ class Counter1 extends Component<Props> {
     return (<div>
       <p>{this.props.number}</p>
       <button onClick={()=>this.props.add()}>加一</button>
+      <button onClick={()=>this.props.asyncAdd()}>异步加一</button>
       <button onClick={()=>this.props.goto({pathname:'/counter2'})}>跳转到c2</button>
     </div>)
   }
